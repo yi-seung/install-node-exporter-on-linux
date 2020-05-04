c1f66c73910faa864da03be39e820eda08fbb7bc
admin <admin@localhost.localdomain>
2020-05-04 19:01:32 +0900

    UPDATE

--- bin/install-node-exporter.sh
+++ bin/install-node-exporter.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 sudo yum update -y
 sudo yum install -y wget
-wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+if ! test -e ./node_exporter-0.18.1.linux-amd64.tar.gz
+	then wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+fi
+
 tar zxvf node_exporter-0.18.1.linux-amd64.tar.gz
 cd node_exporter-0.18.1.linux-amd64
 ls
